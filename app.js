ce69e70e6db6f6e130f084154ca2b90b69e6695c
umair-aptech <umairmohiuddin@aptechnorth.edu.pk>
2025-07-18 17:24:57 +0500

    type writer added

---
+++ app.js
@@ -0,0 +1,15 @@
+// multi -Text
+var typed = new Typed(".multi-text", {
+  strings: [
+                'Website Designer', 
+                'Website Developer', 
+                'Photo Editor', 
+                'Video Editor', 
+                'Business Developer', 
+                'Data Analysis'
+            ],
+  typeSpeed: 70,
+  backSpeed: 70,
+  backDelay: 1000,
+  loop: true
+});
